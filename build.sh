61f67cc64d4f8b92a0a78e4234e6fc112af0015a
Abel Marrero Santos <amarrero@gmail.com>
2021-03-11 16:14:10 -0500

    Version 1.1.0 with Vernemq 1.11.0

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build . -t amtech/vernemq || exit 1
-docker tag amtech/vernemq dockerregistry.amtech.mx:5000/vernemq:1.0.7
-docker push dockerregistry.amtech.mx:5000/vernemq:1.0.7
+docker tag amtech/vernemq dockerregistry.amtech.mx:5000/vernemq:1.1.0
+docker push dockerregistry.amtech.mx:5000/vernemq:1.1.0
