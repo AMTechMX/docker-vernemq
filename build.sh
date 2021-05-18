61857c27028a188540eb9545ec067567087f3aa6
Abel Marrero Santos <amarrero@gmail.com>
2021-05-18 10:53:52 -0400

    Redis.lua changes

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build . -t amtech/vernemq || exit 1
-docker tag amtech/vernemq dockerregistry.amtech.mx:5000/vernemq:1.1.0
-docker push dockerregistry.amtech.mx:5000/vernemq:1.1.0
+docker tag amtech/vernemq dockerregistry.amtech.mx:5000/vernemq:1.2.0
+docker push dockerregistry.amtech.mx:5000/vernemq:1.2.0
